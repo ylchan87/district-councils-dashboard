f4759a83aac47b941f8fde59fff6abc31b95223f
Brian Leung <brianleung1017@gmail.com>
2019-04-13 01:09:32 +0800

    Updates

---
+++ precache-manifest.5909fa576717a3c6ba28b072e6a27d7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32a2c9fb4360314b4ba9",
+    "url": "/district-councils-dashboard/static/js/runtime~main.8cf94a55.js"
+  },
+  {
+    "revision": "e4449259841c7b39d984",
+    "url": "/district-councils-dashboard/static/js/main.bf1e74ef.chunk.js"
+  },
+  {
+    "revision": "45752353b9644380b7b6",
+    "url": "/district-councils-dashboard/static/js/2.8b5d4af2.chunk.js"
+  },
+  {
+    "revision": "e4449259841c7b39d984",
+    "url": "/district-councils-dashboard/static/css/main.f6cf2da4.chunk.css"
+  },
+  {
+    "revision": "bded6bfc5d1056cf6f2cd54b74bf944f",
+    "url": "/district-councils-dashboard/index.html"
+  }
+];
