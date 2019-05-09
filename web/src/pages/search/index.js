f881abe3fcfd359b548a1c5b4abab53310647e11
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-09 15:16:16 +0000

    Deploy cswbrian/district-councils-dashboard to github.com/cswbrian/district-councils-dashboard.git:gh-pages

---
+++ web/src/pages/search/index.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { withStyles } from '@material-ui/core/styles'
+import createMuiTheme from '../../ui/theme'
+import { getAllFeaturesFromPoint } from '../../utils/features'
+import Input from '@material-ui/core/Input';
+
+
+
+
+import dc2003 from '../../data/DCCA_2003'
+import dc2007 from '../../data/DCCA_2007'
+import dc2011 from '../../data/DCCA_2011'
+import dc2015 from '../../data/DCCA_2015'
+import dc2019 from '../../data/DCCA_2019'
+import electors from '../../data/electors'
+import candidates from '../../data/initium-full-dc-candidates'
+
+import * as AddressParser from 'hk-address-parser-lib';
+
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+
+const theme = createMuiTheme
+
+const styles = theme => ({
+  content: {
+    flexGrow: 1
+  },
+  toolbar: theme.mixins.toolbar,
+  search1: {
+    position: 'absolute',
+    top: '15%',
+    width: '50%'
+  },
+  search2: {
+    position: 'absolute',
+    top: '15%',
+    left: '55%',
+    width: '45%'
+  }
+})
+
+class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      autoCompleteList: [],
+      autoCompleteNameList: [],
+    }
+  }
+
+  async componentDidMount() {
+
+  }
+
+  async onAddressFieldChanged(evt) {
+    const { value } = evt.target;
+    const records = await AddressParser.parse(value);
+    this.setState({
+      autoCompleteList: records,
+    })
+  }
+
+  async onNameFieldChanged(evt) {
+    const { value } = evt.target;
+    const records = candidates.filter(candidate => candidate.Candidate_name.includes(value))
+    console.log( records );
+    this.setState({
+      autoCompleteNameList: records
+    })
+  }
+
+  render() {
+    const { classes } = this.props
+    const { autoCompleteList } = this.state;
+    const { autoCompleteNameList } = this.state;
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <main className={classes.content}>
+        <div className={classes.container}>
+      <Input
+        defaultValue="Search Address"
+        className={classes.search1}
+        inputProps={{
+          'aria-label': 'Description',
+        }}
+        onChange={this.onAddressFieldChanged.bind(this)}
+      />
+
+      <Input
+        placeholder="Search People"
+        className={classes.search2}
+        inputProps={{
+          'aria-label': 'Description',
+        }}
+        onChange={this.onNameFieldChanged.bind(this)}
+      />
+      { autoCompleteList.map( (address, index) => (<div key={index}><p>{address.fullAddress()}</p></div>))}
+      { autoCompleteNameList.map( (candidate, index) => (<div key={index}><p>{candidate.ID} {candidate.Year} {candidate.Constituency_code}</p></div>))}
+    </div>
+
+        </main>
+
+      </MuiThemeProvider>
+    )
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(SearchPage)
