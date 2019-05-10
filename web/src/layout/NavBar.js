68e74d2a8367e59c2c9b1641d48e707839559688
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-10 18:06:59 +0000

    Deploy cswbrian/district-councils-dashboard to github.com/cswbrian/district-councils-dashboard.git:gh-pages

---
+++ web/src/layout/NavBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { NavLink } from "react-router-dom";
+
+const styles = theme => ({
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    }
+})
+
+class NavBar extends Component {
+    render() {
+        const { classes } = this.props
+        return (
+            <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <Typography variant="h6" color="inherit" noWrap>
+              District Council Dashboard
+              </Typography>
+              <NavLink to="/"><Button color="inherit">Search</Button></NavLink>
+              <NavLink to="/map"><Button color="inherit">Map</Button></NavLink>
+            </Toolbar>
+          </AppBar>
+        )
+    }
+}
+
+export default withStyles(styles, { withTheme: true })(NavBar)
